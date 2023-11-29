a3a4be55cac7636c1623da96f3fc3a0693f7e48a
Aurélien Rainone <aurelien.rainone@gmail.com>
2023-11-29 19:34:16 +0100

    emu/hwio: add register and bank handling via tags

---
+++ emu/hwio/reg.go
@@ -0,0 +1,71 @@
+package hwio
+
+import "fmt"
+
+type RegFlags uint8
+
+const (
+	RegFlagReadOnly RegFlags = (1 << iota)
+	RegFlagWriteOnly
+)
+
+type Reg8 struct {
+	Name   string
+	Value  uint8
+	RoMask uint8
+
+	Flags   RegFlags
+	ReadCb  func(val uint8) uint8
+	WriteCb func(old uint8, val uint8)
+}
+
+func (reg Reg8) String() string {
+	s := fmt.Sprintf("%s{%02x", reg.Name, reg.Value)
+	if reg.ReadCb != nil {
+		s += ",r!"
+	}
+	if reg.WriteCb != nil {
+		s += ",w!"
+	}
+	return s + "}"
+}
+
+func (reg *Reg8) write(val uint8, romask uint8) {
+	romask = romask | reg.RoMask
+	old := reg.Value
+	reg.Value = (reg.Value & romask) | (val &^ romask)
+	if reg.WriteCb != nil {
+		reg.WriteCb(old, reg.Value)
+	}
+}
+
+func hex16(val uint16) string {
+	return fmt.Sprintf("%04x", val)
+}
+
+func (reg *Reg8) Write8(addr uint16, val uint8) {
+	if reg.Flags&RegFlagReadOnly != 0 {
+		// log.ModHwIo.WithFields(log.Fields{
+		// 	"name": reg.Name,
+		// 	"addr": hex16(addr),
+		// }).Error("invalid Write16 to readonly reg")
+		// return
+		panic("invalid Write8 to readonly reg")
+	}
+	reg.write(val, 0)
+}
+
+func (reg *Reg8) Read8(addr uint16) uint8 {
+	if reg.Flags&RegFlagWriteOnly != 0 {
+		// log.ModHwIo.WithFields(log.Fields{
+		// 	"name": reg.Name,
+		// 	"addr": emu.Hex32(addr),
+		// }).Error("invalid Read16 from writeonly reg")
+		// return 0
+		panic("invalid Read8 to readonly reg")
+	}
+	if reg.ReadCb != nil {
+		return reg.ReadCb(reg.Value)
+	}
+	return reg.Value
+}
