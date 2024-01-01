c9d5b45bdf5461077de2adcad6a7d1f6b7b98d5f
Aurélien Rainone <aurelien.rainone@gmail.com>
2024-01-01 19:26:10 +0100

    emu/hwio: add 8-bit bitops

--- emu/hwio/bitops.go
+++ emu/hwio/bitops.go
@@ -1,5 +1,31 @@
 package hwio
 
+// 8-bit operations
+func GetBit8(v uint8, n uint) bool {
+	return GetBiti8(v, n) != 0
+}
+
+func GetBiti8(v uint8, n uint) uint8 {
+	return v >> (n) & 0x01
+}
+
+func SetBit8(v *uint8, n uint) {
+	*v |= (1 << n)
+}
+
+func ClearBit8(v *uint8, n uint) {
+	*v &= ^(1 << n)
+}
+
+func FlipBit8(v *uint8, n uint) {
+	*v ^= (1 << n)
+}
+
+func ClearBits8(v *uint8, mask uint8) {
+	*v &= ^mask
+}
+
+// 16-bit operations
 func GetBit16(v uint16, n uint) bool {
 	return GetBiti16(v, n) != 0
 }
@@ -8,18 +34,18 @@ func GetBiti16(v uint16, n uint) uint16 {
 	return v >> (n) & 0x01
 }
 
-func SetBit(v *uint16, n uint) {
+func SetBit16(v *uint16, n uint) {
 	*v |= (1 << n)
 }
 
-func ClearBit(v *uint16, n uint) {
+func ClearBit16(v *uint16, n uint) {
 	*v &= ^(1 << n)
 }
 
-func FlipBit(v *uint16, n uint) {
+func FlipBit16(v *uint16, n uint) {
 	*v ^= (1 << n)
 }
 
-func ClearBits(v *uint16, mask uint16) {
+func ClearBits16(v *uint16, mask uint16) {
 	*v &= ^mask
 }
