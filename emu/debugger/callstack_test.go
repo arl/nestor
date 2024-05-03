06f983d2434e3b8ec4a2a92d729d1625d6309dc3
Aurélien Rainone <aurelien.rainone@gmail.com>
2024-05-03 12:26:06 +0200

    emu/debugger: add basic callstack test

--- emu/debugger/callstack_test.go
+++ emu/debugger/callstack_test.go
@@ -1,6 +1,10 @@
 package debugger
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
 
 func TestCallStack(t *testing.T) {
 	t.Run("simple", func(t *testing.T) {
@@ -9,17 +13,24 @@ func TestCallStack(t *testing.T) {
 		cstack.push(0xC801, 0xCBAE, 0xC804, sffNone)
 
 		fi := cstack.build(0xF099)
-
-		for _, f := range fi {
-			t.Logf("%s", f)
+		want := []frameInfo{
+			{"CBAE", "$F099"},
+			{"C7E7", "$C801"},
+			{"[bottom of stack]", "$C7C2"},
+		}
+		if diff := cmp.Diff(fi, want); diff != "" {
+			t.Fatalf("callstack differs (-want +got):\n%s", diff)
 		}
 	})
 
 	t.Run("empty", func(t *testing.T) {
 		var cstack callStack
 		fi := cstack.build(0xF099)
-		for _, f := range fi {
-			t.Logf("%s", f)
+		want := []frameInfo{
+			{"[bottom of stack]", "$F099"},
+		}
+		if diff := cmp.Diff(fi, want); diff != "" {
+			t.Fatalf("callstack differs (-want +got):\n%s", diff)
 		}
 	})
 }
