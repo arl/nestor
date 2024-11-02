8b4b1496690f9cb845ae9fc40b6d6266edd4887e
Aurélien Rainone <aurelien.rainone@gmail.com>
2024-11-02 13:55:35 +0100

    hw: simplify memory mapping and mapper code

--- hw/mappers/uxrom.go
+++ hw/mappers/uxrom.go
@@ -1,8 +1,6 @@
 package mappers
 
 import (
-	"fmt"
-
 	"nestor/hw"
 	"nestor/hw/hwio"
 	"nestor/ines"
@@ -16,27 +14,20 @@ var UxROM = hw.MapperDesc{
 type uxrom struct {
 	rom *ines.Rom
 
+	PRGRAM hwio.Mem `hwio:"offset=0x6000,size=0x2000"`
+
 	// switchable PRGROM bank
-	PRGROM hwio.Manual `hwio:"offset=0x0000,size=0x8000"`
-	cur    uint        // current bank
+	PRGROM hwio.Manual `hwio:"offset=0x8000,size=0x8000"`
+	cur    uint32      // current bank
 }
 
 func (m *uxrom) ReadPRGROM(addr uint16, _ bool) uint8 {
-	var bank uint32
+	banknum := m.cur
 	if addr >= 0xC000 {
-		bank = uint32(m.rom.PRGROMSlots() - 1)
-	} else {
-		bank = uint32(m.cur)
+		banknum = uint32(m.rom.PRGROMSlots() - 1)
 	}
-	romaddr := (bank * 0x4000) + uint32(addr&0x3FFF)
-	val := m.rom.PRGROM[romaddr]
-	modMapper.DebugZ("PRGROM read").
-		Hex16("addr", addr).
-		Hex32("romaddr", romaddr).
-		Uint("bank", m.cur).
-		Hex8("val", val).
-		End()
-	return val
+	romaddr := (banknum * 0x4000) + uint32(addr&0x3FFF)
+	return m.rom.PRGROM[romaddr]
 }
 
 func (m *uxrom) WritePRGROM(addr uint16, val uint8) {
@@ -49,67 +40,25 @@ func (m *uxrom) WritePRGROM(addr uint16, val uint8) {
 	//      ++++- Select 16 KB PRG ROM bank for CPU $8000-$BFFF
 	//            (UNROM uses bits 2-0; UOROM uses bits 3-0)
 	prev := m.cur
-	m.cur = uint(val & 0b111)
+	m.cur = uint32(val & 0b111)
 	modMapper.DebugZ("PRGROM bank switch").
-		Uint("prev", prev).
-		Uint("new", m.cur).
+		Uint32("prev", prev).
+		Uint32("new", m.cur).
 		End()
 }
 
 func loadUxROM(rom *ines.Rom, cpu *hw.CPU, ppu *hw.PPU) error {
-	// CPU memory space mapping.
-
-	// PRG-RAM (always provide it, though it should only be for 'Family basic').
-	PRGRAM := make([]uint8, 0x2000)
-
-	cpu.Bus.MapMemorySlice(0x6000, 0x7FFF, PRGRAM, false)
-	modMapper.DebugZ("map PRGRAM to cpu").
-		String("range", "0x6000-0x7FFF").
-		String("src", "slice").
-		End()
-
-	uxrom := &uxrom{
-		rom: rom,
-		cur: 0,
-	}
+	uxrom := &uxrom{rom: rom}
 	hwio.MustInitRegs(uxrom)
-	cpu.Bus.MapBank(0x8000, uxrom, 0)
 
-	// PPU memory space mapping.
-	// Nametables.
-	switch rom.Mirroring() {
-	case ines.HorzMirroring: // A A B B
-		ppu.SetMirroring(hw.HorzMirroring)
-	case ines.VertMirroring: // A B A B
-		ppu.SetMirroring(hw.VertMirroring)
-	default:
-		return fmt.Errorf("unexpected mirroring: %d", rom.Mirroring())
-	}
+	// CPU mapping.
+	cpu.Bus.MapBank(0x0000, uxrom, 0)
 
-	// continuer ici avec le log
-	// Copy CHRROM to Pattern Tables.
-	switch len(rom.CHRROM) {
-	case 0x0000:
-		// Some roms have no CHR-ROM, allocate mem anyway.
-		// XXX: not sure about that
-		// copy(ppu.PatternTables.Data, make([]uint8, 0x2000))
-		// modMapper.DebugZ("map CHR-ROM to pattern tables").
-		// 	String("range", "0x0000-0x2000").
-		// 	String("src", "rom.CHR-ROM").
-		// 	End()
-		//panic("not sure about that, check comment")
-
-	case 0x2000:
-		copy(ppu.PatternTables.Data, rom.CHRROM)
-		modMapper.DebugZ("map CHR-ROM to pattern tables").
-			String("range", "0x0000-0x2000").
-			String("src", "rom.CHR-ROM").
-			End()
-	default:
-		return fmt.Errorf("unexpected CHRROM size: 0x%x", len(rom.CHRROM))
-	}
+	// PPU mapping.
+	hw.SetNTMirroring(ppu, rom.Mirroring())
+	copy(ppu.PatternTables.Data, rom.CHRROM)
+	return nil
 
 	// TODO: load and map PRG-RAM if present in cartridge.
 	// TODO: load and map CHR-RAM if present in cartridge.
-	return nil
 }
--- hw/ppu.go
+++ hw/ppu.go
@@ -90,48 +90,6 @@ func (p *PPU) SetFrameBuffer(framebuf []byte) {
 	p.framebuf = unsafe.Slice((*uint32)(unsafe.Pointer(&framebuf[0])), len(framebuf)/4)
 }
 
-type Mirroring int
-
-const (
-	HorzMirroring Mirroring = iota
-	VertMirroring
-)
-
-// called from the mapper.
-func (p *PPU) SetMirroring(m Mirroring) {
-	A := p.Nametables[:0x400]
-	B := p.Nametables[0x400:0x800]
-
-	// NameTables
-	switch m {
-	case HorzMirroring:
-		// A A B B
-		p.Bus.MapMemorySlice(0x2000, 0x23FF, A, false)
-		p.Bus.MapMemorySlice(0x2400, 0x27FF, A, false)
-		p.Bus.MapMemorySlice(0x2800, 0x2BFF, B, false)
-		p.Bus.MapMemorySlice(0x2C00, 0x2FFF, B, false)
-
-		// nametables mirrors
-		p.Bus.MapMemorySlice(0x3000, 0x33FF, A, false)
-		p.Bus.MapMemorySlice(0x3400, 0x37FF, A, false)
-		p.Bus.MapMemorySlice(0x3800, 0x3BFF, B, false)
-		p.Bus.MapMemorySlice(0x3C00, 0x3EFF, B, false)
-
-	case VertMirroring:
-		// A B A B
-		p.Bus.MapMemorySlice(0x2000, 0x23FF, A, false)
-		p.Bus.MapMemorySlice(0x2400, 0x27FF, B, false)
-		p.Bus.MapMemorySlice(0x2800, 0x2BFF, A, false)
-		p.Bus.MapMemorySlice(0x2C00, 0x2FFF, B, false)
-
-		// nametables mirrors
-		p.Bus.MapMemorySlice(0x3000, 0x33FF, A, false)
-		p.Bus.MapMemorySlice(0x3400, 0x37FF, B, false)
-		p.Bus.MapMemorySlice(0x3800, 0x3BFF, A, false)
-		p.Bus.MapMemorySlice(0x3C00, 0x3EFF, B, false)
-	}
-}
-
 func (p *PPU) Reset() {
 	p.Scanline = -1
 	p.Cycle = 339
