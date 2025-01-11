e4445c43aa49e09258838f4cefc156840490cd82
Aurélien Rainone <aurelien.rainone@gmail.com>
2025-01-11 17:22:54 +0100

    ines: decode more fields of the iNES2.0 format

--- ines/ines.go
+++ ines/ines.go
@@ -1,5 +1,5 @@
-// package ines implements a Reader for roms inz the iNES file format, used for
-// for the distribution of NES binary programs.
+// package ines implements a Reader for roms in the iNES or NES 2.0 file format,
+// used for for the distribution of NES binary programs.
 package ines
 
 import (
@@ -15,7 +15,7 @@ type Rom struct {
 	CHRROM  []uint8 // CHR is PRG ROM data (size is a multiple of 8k)
 }
 
-func yesno(b bool) string {
+func yn(b bool) string {
 	if b {
 		return "yes"
 	}
@@ -23,13 +23,26 @@ func yesno(b bool) string {
 }
 
 func (rom *Rom) PrintInfos(w io.Writer) {
-	fmt.Fprintf(w, "iNES2.0 : %s\n", yesno(rom.IsNES20()))
-	fmt.Fprintf(w, "Mapper: %d\n", rom.Mapper())
-	fmt.Fprintf(w, "PRG ROM: %dx16k\n", rom.header.PRGROMSlots())
-	fmt.Fprintf(w, "CHR ROM: %dx8k\n", rom.header.CHRROMSlots())
-	fmt.Fprintf(w, "Nametable mirroring: %s\n", rom.Mirroring())
-	fmt.Fprintf(w, "Trainer: %s\n", yesno(rom.HasTrainer()))
-	fmt.Fprintf(w, "Persistent: %s\n", yesno(rom.HasPersistence()))
+	fmt.Fprintf(w, "|iNES2.0                | % 14s |\n", yn(rom.IsNES20()))
+	if rom.IsNES20() {
+		fmt.Fprintf(w, "|Region                 | % 14s |\n", rom.Region())
+	}
+	fmt.Fprintf(w, "|Mapper                 | % 14d |\n", rom.Mapper())
+	if rom.IsNES20() {
+		fmt.Fprintf(w, "|Submapper              | % 14d |\n", rom.SubMapper())
+	}
+	fmt.Fprintf(w, "|PRG ROM                | % 8d x 16k |\n", rom.header.PRGROMSlots())
+	fmt.Fprintf(w, "|CHR ROM                | % 9d x 8k |\n", rom.header.CHRROMSlots())
+	if rom.IsNES20() {
+		fmt.Fprintf(w, "|PRG RAM                | % 13dk |\n", rom.header.PRGRAMSize()/1024)
+		fmt.Fprintf(w, "|PRG NVRAM              | % 13dk |\n", rom.header.PRGNVRAMSize()/1024)
+		fmt.Fprintf(w, "|CHR RAM                | % 13dk |\n", rom.header.CHRRAMSize()/1024)
+		fmt.Fprintf(w, "|CHR NVRAM              | % 13dk |\n", rom.header.CHRNVRAMSize()/1024)
+	}
+	fmt.Fprintf(w, "|Nametable mirroring    | % 14s |\n", rom.Mirroring())
+	fmt.Fprintf(w, "|Alternative nametable  | % 14s |\n", yn(rom.HasAltNametables()))
+	fmt.Fprintf(w, "|Trainer                | % 14s |\n", yn(rom.HasTrainer()))
+	fmt.Fprintf(w, "|Persistent             | % 14s |\n", yn(rom.HasPersistence()))
 }
 
 // ReadRom loads a rom from an iNES file.
@@ -87,6 +100,12 @@ func Decode(buf []byte) (*Rom, error) {
 
 const Magic = "NES\x1a"
 
+type header struct {
+	raw   [16]byte
+	prgsz int
+	chrsz int
+}
+
 func (hdr *header) decode(p []byte) error {
 	if len(p) < 16 {
 		return fmt.Errorf("too small, needs 16 bytes")
@@ -98,13 +117,11 @@ func (hdr *header) decode(p []byte) error {
 
 	hdr.prgsz = int(hdr.raw[4])
 	hdr.chrsz = int(hdr.raw[5])
+	if hdr.IsNES20() {
+		hdr.prgsz |= int(hdr.raw[9]&0x0F) << 8
+		hdr.chrsz |= int(hdr.raw[9] & 0xF0)
+	}
 	return nil
-}
-
-type header struct {
-	raw   [16]byte
-	prgsz int
-	chrsz int
 }
 
 // PRGROMSlots returns the number of 16kB slots of PRGROM.
@@ -117,9 +134,73 @@ func (hdr *header) CHRROMSlots() int {
 	return hdr.chrsz
 }
 
+// PRGRAMSize returns the size of the PRG-RAM (volatile).
+func (hdr *header) PRGRAMSize() int {
+	if hdr.IsNES20() {
+		return 64 << int(hdr.raw[10]&0x0F)
+	}
+	return 0
+}
+
+// PRGNVRAMSize returns the size of the PRG-NVRAM/	EEPROM (non-volatile).
+func (hdr *header) PRGNVRAMSize() int {
+	if hdr.IsNES20() {
+		return 64 << int(hdr.raw[10]>>4)
+	}
+	return 0
+}
+
+// CHRRAMSize returns the size of the CHR-RAM (volatile).
+func (hdr *header) CHRRAMSize() int {
+	if hdr.IsNES20() {
+		return 64 << int(hdr.raw[11]&0x0F)
+	}
+	return 0
+}
+
+// CHRNVRAMSize returns the size of the CHR-NVRAM (non-volatile).
+func (hdr *header) CHRNVRAMSize() int {
+	if hdr.IsNES20() {
+		return 64 << int(hdr.raw[11]>>4)
+	}
+	return 0
+}
+
+//go:generate go run golang.org/x/tools/cmd/stringer -type=Region
+
+// Region indicates the region where the rom was released, for non-homebrew games.
+type Region byte
+
+const (
+	NTSC Region = iota
+	PAL
+	Multiple
+	Dendy
+
+	Unspecified = 0xFF
+)
+
+func (hdr *header) Region() Region {
+	if hdr.IsNES20() {
+		return Region(hdr.raw[10] & 0x03)
+	}
+	return Unspecified
+}
+
 // Mapper returns the mapper number.
 func (hdr *header) Mapper() uint16 {
+	if hdr.IsNES20() {
+		return uint16(hdr.raw[8]&0x0F) | uint16(hdr.raw[7]&0xF0) | uint16(hdr.raw[6]>>4)
+	}
 	return uint16(hdr.raw[7]&0xF0) | uint16(hdr.raw[6]>>4)
+}
+
+// SubMapper returns the submapper number.
+func (hdr *header) SubMapper() uint8 {
+	if hdr.IsNES20() {
+		return hdr.raw[8] >> 4
+	}
+	return 0
 }
 
 // IsNES20 reports whether the rom is in the NES2.0 format.
@@ -141,7 +222,7 @@ const (
 )
 
 func (hdr *header) Mirroring() NTMirroring {
-	if hdr.raw[6]&(1<<0) != 0 {
+	if hdr.raw[6]&0x01 == 0x01 {
 		return VertMirroring
 	}
 	return HorzMirroring
@@ -149,10 +230,15 @@ func (hdr *header) Mirroring() NTMirroring {
 
 // HasPersistence indicates the presence of persistent saved memory in the rom.
 func (hdr *header) HasPersistence() bool {
-	return hdr.raw[6]&(1<<1) != 0
+	return hdr.raw[6]&0x02 == 0x02
 }
 
-// Has Trainer indicates the presence of a trainer section in the rom.
+// HasTrainer indicates the presence of a trainer section in the rom.
 func (hdr *header) HasTrainer() bool {
-	return hdr.raw[6]&(1<<2) != 0
+	return hdr.raw[6]&0x04 == 0x04
+}
+
+// HasAltNametables indicates a mapper-specific alternative nametable layout
+func (hdr *header) HasAltNametables() bool {
+	return hdr.raw[6]&0x08 == 0x08
 }
