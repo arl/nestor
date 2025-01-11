6f0e8df0d2e1d0e16a3383c9ef0591d827a99c05
Aurélien Rainone <aurelien.rainone@gmail.com>
2025-01-11 14:39:58 +0100

    hw/mappers: revert to previous nt mirroring code

--- hw/mappers/base.go
+++ hw/mappers/base.go
@@ -60,18 +60,64 @@ func (b *base) setNametables(ntidx0, ntidx1, ntidx2, ntidx3 uint16) {
 	b.mapNametable(3, ntidx3)
 }
 
-func (b *base) setNametableMirroring(m ines.Mirroring) {
-	b.cpu.Bus.Unmap(0x2000, 0x3EFF)
+func (b *base) setNametableMirroring(m ines.NTMirroring) {
+	// b.cpu.Bus.Unmap(0x2000, 0x3EFF)
+	A := b.ppu.Nametables[:0x400]
+	B := b.ppu.Nametables[0x400:0x800]
 
 	switch m {
 	case ines.HorzMirroring:
-		b.setNametables(0, 0, 1, 1)
+		// b.setNametables(0, 0, 1, 1)
+		b.ppu.Bus.MapMemorySlice(0x2000, 0x23FF, A, false)
+		b.ppu.Bus.MapMemorySlice(0x2400, 0x27FF, A, false)
+		b.ppu.Bus.MapMemorySlice(0x2800, 0x2BFF, B, false)
+		b.ppu.Bus.MapMemorySlice(0x2C00, 0x2FFF, B, false)
+
+		// nametables mirrors
+		b.ppu.Bus.MapMemorySlice(0x3000, 0x33FF, A, false)
+		b.ppu.Bus.MapMemorySlice(0x3400, 0x37FF, A, false)
+		b.ppu.Bus.MapMemorySlice(0x3800, 0x3BFF, B, false)
+		b.ppu.Bus.MapMemorySlice(0x3C00, 0x3EFF, B, false)
+
 	case ines.VertMirroring:
-		b.setNametables(0, 1, 0, 1)
+		// b.setNametables(0, 1, 0, 1)
+		// A B A B
+		b.ppu.Bus.MapMemorySlice(0x2000, 0x23FF, A, false)
+		b.ppu.Bus.MapMemorySlice(0x2400, 0x27FF, B, false)
+		b.ppu.Bus.MapMemorySlice(0x2800, 0x2BFF, A, false)
+		b.ppu.Bus.MapMemorySlice(0x2C00, 0x2FFF, B, false)
+
+		// nametables mirrors
+		b.ppu.Bus.MapMemorySlice(0x3000, 0x33FF, A, false)
+		b.ppu.Bus.MapMemorySlice(0x3400, 0x37FF, B, false)
+		b.ppu.Bus.MapMemorySlice(0x3800, 0x3BFF, A, false)
+		b.ppu.Bus.MapMemorySlice(0x3C00, 0x3EFF, B, false)
 	case ines.OnlyAScreen:
-		b.setNametables(0, 0, 0, 0)
+		// b.setNametables(0, 0, 0, 0)
+		// A A A A
+		b.ppu.Bus.MapMemorySlice(0x2000, 0x23FF, A, false)
+		b.ppu.Bus.MapMemorySlice(0x2400, 0x27FF, A, false)
+		b.ppu.Bus.MapMemorySlice(0x2800, 0x2BFF, A, false)
+		b.ppu.Bus.MapMemorySlice(0x2C00, 0x2FFF, A, false)
+
+		// nametables mirrors
+		b.ppu.Bus.MapMemorySlice(0x3000, 0x33FF, A, false)
+		b.ppu.Bus.MapMemorySlice(0x3400, 0x37FF, A, false)
+		b.ppu.Bus.MapMemorySlice(0x3800, 0x3BFF, A, false)
+		b.ppu.Bus.MapMemorySlice(0x3C00, 0x3EFF, A, false)
 	case ines.OnlyBScreen:
-		b.setNametables(1, 1, 1, 1)
+		// b.setNametables(1, 1, 1, 1)
+		// A A A A
+		b.ppu.Bus.MapMemorySlice(0x2000, 0x23FF, B, false)
+		b.ppu.Bus.MapMemorySlice(0x2400, 0x27FF, B, false)
+		b.ppu.Bus.MapMemorySlice(0x2800, 0x2BFF, B, false)
+		b.ppu.Bus.MapMemorySlice(0x2C00, 0x2FFF, B, false)
+
+		// nametables mirrors
+		b.ppu.Bus.MapMemorySlice(0x3000, 0x33FF, B, false)
+		b.ppu.Bus.MapMemorySlice(0x3400, 0x37FF, B, false)
+		b.ppu.Bus.MapMemorySlice(0x3800, 0x3BFF, B, false)
+		b.ppu.Bus.MapMemorySlice(0x3C00, 0x3EFF, B, false)
 	default:
 		panic("unsupported mirroring: " + strconv.Itoa(int(m)))
 	}
