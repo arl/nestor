70121e39d29610019d0692d5a6778743a22d53ab
Aurélien Rainone <aurelien.rainone@gmail.com>
2025-04-08 09:34:19 +0200

    hw/mappers: CNROM uses mappers.base

--- hw/mappers/cnrom.go
+++ hw/mappers/cnrom.go
@@ -1,7 +1,5 @@
 package mappers
 
-import "nestor/hw/hwio"
-
 var CNROM = MapperDesc{
 	Name:         "CNROM",
 	Load:         loadCNROM,
@@ -12,26 +10,23 @@ var CNROM = MapperDesc{
 type cnrom struct {
 	*base
 
-	/* CPU */
-	PRGROM hwio.Device `hwio:"offset=0x8000,size=0x8000,rcb,wcb"`
-
-	/* PPU */
-	PatternTables hwio.Mem `hwio:"bank=1,offset=0x0000,size=0x2000,readonly"`
-	chrbank       uint32
+	chrbank uint32
 
 	busConflicts bool
 }
 
-func (m *cnrom) ReadPRGROM(addr uint16) uint8 {
-	addr -= 0x8000
-	addr &= uint16(len(m.rom.PRGROM) - 1)
-	return m.rom.PRGROM[addr]
-}
-
 func (m *cnrom) WritePRGROM(addr uint16, val uint8) {
+	old := val
 	if m.busConflicts {
-		val &= m.ReadPRGROM(addr)
+		old = m.cpu.Bus.Peek8(addr)
+		val &= old
 	}
+	modMapper.DebugZ("WritePRGROM").
+		Hex16("addr", addr).
+		Hex8("old", old).
+		Hex8("val", val).
+		Bool("conflicts", m.busConflicts).
+		End()
 
 	// 7  bit  0
 	// ---- ----
@@ -40,10 +35,9 @@ func (m *cnrom) WritePRGROM(addr uint16, val uint8) {
 	// ++++-++++- Select 8 KB CHR ROM bank for PPU $0000-$1FFF
 	// CNROM only uses lowest 2 bits
 	prev := m.chrbank
-	m.chrbank = uint32(val & 0b11)
+	m.chrbank = uint32(val) & 0b11
 	if prev != m.chrbank {
-		m.copyCHRROM(m.PatternTables.Data, m.chrbank)
-		modMapper.InfoZ("CHRROM bank switch").String("mapper", m.desc.Name).Uint32("prev", prev).Uint32("new", m.chrbank).End()
+		m.selectCHRROMPage8KB(int(m.chrbank))
 	}
 }
 
@@ -52,22 +46,12 @@ func loadCNROM(b *base) error {
 		base:         b,
 		busConflicts: b.rom.SubMapper() == 2,
 	}
-	hwio.MustInitRegs(cnrom)
-
-	b.cpu.Bus.MapBank(0x0000, cnrom, 0)
-
-	if b.rom.PRGRAMSize() > 0 {
-		b.cpu.Bus.MapMem(0x6000, &hwio.Mem{
-			Name:  "PRGRAM",
-			VSize: 0x2000,
-			Data:  make([]byte, b.rom.PRGRAMSize()),
-		})
-	}
+	b.init(cnrom.WritePRGROM)
 
 	// PPU mapping.
 	b.setNTMirroring(b.rom.Mirroring())
-	b.ppu.Bus.MapBank(0x0000, cnrom, 1)
-	b.copyCHRROM(cnrom.PatternTables.Data, 0)
+	b.selectCHRROMPage8KB(0)
+	b.selectPRGPage32KB(0)
 
 	return nil
 
