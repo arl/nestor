a75dafe74e211917bebb06ee9504fc92b9661849
Aurélien Rainone <aurelien.rainone@gmail.com>
2025-10-06 21:04:09 +0200

    ui: nicer pause buttons

--- ui/helpers.go
+++ ui/helpers.go
@@ -2,29 +2,98 @@ package ui
 
 import (
 	"bytes"
+	"fmt"
+	"image/color"
+	"sync"
 	"unsafe"
 
+	"github.com/ebitenui/ebitenui/image"
 	"github.com/ebitenui/ebitenui/widget"
-	"golang.org/x/image/colornames"
+	"github.com/hajimehoshi/ebiten/v2/text/v2"
+	"golang.org/x/image/font/gofont/goregular"
 )
 
+var buttonImage = sync.OnceValue(func() *widget.ButtonImage {
+	return &widget.ButtonImage{
+		Idle: image.NewBorderedNineSliceColor(
+			color.NRGBA{R: 170, G: 170, B: 180, A: 255},
+			color.NRGBA{90, 90, 90, 255},
+			3),
+		Hover: image.NewBorderedNineSliceColor(
+			color.NRGBA{R: 130, G: 130, B: 150, A: 255},
+			color.NRGBA{70, 70, 70, 255},
+			3),
+		Pressed: image.NewAdvancedNineSliceColor(
+			color.NRGBA{R: 130, G: 130, B: 150, A: 255},
+			image.NewBorder(3, 2, 2, 2, color.NRGBA{70, 70, 70, 255}),
+		),
+	}
+})
+
+var fontFaces = map[float64]text.Face{}
+
+func loadFont(size float64) text.Face {
+	if face, ok := fontFaces[size]; ok {
+		return face
+	}
+
+	s, err := text.NewGoTextFaceSource(bytes.NewReader(goregular.TTF))
+	if err != nil {
+		panic(fmt.Errorf("failed to load font: %s", err))
+	}
+
+	face := &text.GoTextFace{
+		Source: s,
+		Size:   size,
+	}
+	fontFaces[size] = face
+	return face
+}
+
 func stdButton(text string, onclick func(args *widget.ButtonClickedEventArgs)) *widget.Button {
-	return widget.NewButton(
-		widget.ButtonOpts.TextFace(DefaultFont()),
-		widget.ButtonOpts.TextColor(&widget.ButtonTextColor{
-			Idle:    colornames.White,
-			Hover:   colornames.White,
-			Pressed: Mix(colornames.White, colornames.Black, 0.4),
-		}),
-		widget.ButtonOpts.Image(&widget.ButtonImage{
-			Idle:         DefaultNineSlice(colornames.Forestgreen),
-			Hover:        DefaultNineSlice(Mix(colornames.Forestgreen, colornames.White, 0.2)),
-			Pressed:      PressedNineSlice(Mix(colornames.Forestgreen, colornames.Black, 0.4)),
-			PressedHover: PressedNineSlice(Mix(colornames.Forestgreen, colornames.Black, 0.4)),
-		}),
-		widget.ButtonOpts.TextLabel(text),
-		widget.ButtonOpts.ClickedHandler(onclick),
+	var (
+		font   = loadFont(20)
+		button *widget.Button
 	)
+
+	button = widget.NewButton(
+		widget.ButtonOpts.WidgetOpts(
+			widget.WidgetOpts.LayoutData(widget.AnchorLayoutData{
+				HorizontalPosition: widget.AnchorLayoutPositionCenter,
+				VerticalPosition:   widget.AnchorLayoutPositionCenter,
+			}),
+		),
+		widget.ButtonOpts.Image(buttonImage()),
+		widget.ButtonOpts.Text(text, &font, &widget.ButtonTextColor{
+			Idle: color.NRGBA{0xdf, 0xf4, 0xff, 0xff},
+		}),
+		widget.ButtonOpts.TextProcessBBCode(false),
+		widget.ButtonOpts.TextPadding(&widget.Insets{
+			Left:   30,
+			Right:  30,
+			Top:    5,
+			Bottom: 5,
+		}),
+		widget.ButtonOpts.PressedHandler(func(args *widget.ButtonPressedEventArgs) {
+			button.Text().SetPadding(&widget.Insets{Top: 1, Bottom: -1})
+			button.GetWidget().CustomData = true
+		}),
+		widget.ButtonOpts.ReleasedHandler(func(args *widget.ButtonReleasedEventArgs) {
+			button.Text().SetPadding(&widget.Insets{})
+			button.GetWidget().CustomData = false
+		}),
+		widget.ButtonOpts.CursorEnteredHandler(func(args *widget.ButtonHoverEventArgs) {
+			if button.GetWidget().CustomData == true {
+				button.Text().SetPadding(&widget.Insets{Top: 1, Bottom: -1})
+			}
+		}),
+		widget.ButtonOpts.CursorExitedHandler(func(args *widget.ButtonHoverEventArgs) {
+			button.Text().SetPadding(&widget.Insets{})
+		}),
+		widget.ButtonOpts.ClickedHandler(onclick),
+		widget.ButtonOpts.DisableDefaultKeys(),
+	)
+	return button
 }
 
 // sampleBuffer is a wrapper around bytes.Buffer that returns nil instead of
