5269a85045f6c0e9ac132c2f15887fba175d2d1d
Aurélien Rainone <aurelien.rainone@gmail.com>
2025-11-30 08:19:11 +0100

    ui: split application.go. s/appState/state

--- ui/application.go
+++ ui/application.go
@@ -16,107 +16,16 @@ import (
 
 	"nestor/config"
 	"nestor/emu"
-	"nestor/emu/log"
-	"nestor/hw/apu"
 	"nestor/hw/input"
 	"nestor/ines"
 )
 
-var modUI = log.NewModule("ui")
-
-const startwidth = 800
-const startheight = 600
-
-func StartUI(ctx context.Context, cfg config.Config) error {
-	return start(ctx, cfg, "")
-}
-
-func StartROM(ctx context.Context, cfg config.Config, romPath string) error {
-	return start(ctx, cfg, romPath)
-}
-
-func start(ctx context.Context, cfg config.Config, romPath string) error {
-	initResources()
-
-	// Init audio.
-	samples, audioctx, err := initAudio()
-	if err != nil {
-		return fmt.Errorf("initAudio failure: %s", err)
-	}
-
-	// Init video.
-	setMonitor(cfg.Video.Monitor)
-	ebiten.SetWindowTitle("Nestor")
-	ebiten.SetWindowSize(startwidth, startheight)
-	ebiten.SetWindowSizeLimits(startwidth, startheight, -1, -1)
-	ebiten.SetRunnableOnUnfocused(false)
-	if cfg.Video.StartFullscreen {
-		ebiten.SetFullscreen(true)
-	}
-	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
-	ebiten.SetTPS(ebiten.SyncWithFPS)
-	ebiten.SetRunnableOnUnfocused(true)
-
-	app := newApp(ctx, samples, audioctx, cfg)
-
-	if romPath != "" {
-		app.setState("running")
-		if err := app.runRom(romPath); err != nil {
-			return fmt.Errorf("can't run rom: %w", err)
-		}
-	} else {
-		app.setState("main")
-	}
-
-	options := &ebiten.RunGameOptions{
-		SingleThread: false,
-	}
-	if err := ebiten.RunGameWithOptions(app, options); err != nil {
-		return fmt.Errorf("ui failure: %w", err)
-	}
-
-	modUI.InfoZ("ui quitted").End()
-	return nil
-}
-
-func initAudio() (*sampleBuffer, *oto.Context, error) {
-	const audioBufferSize = 1024 // TODO: adjust based on latency.
-	samples := newSampleBuffer(audioBufferSize)
-
-	audioctx, readych, err := oto.NewContext(&oto.NewContextOptions{
-		SampleRate:   apu.MaxSampleRate,
-		ChannelCount: 2,
-		Format:       oto.FormatSignedInt16LE,
-	})
-	if err != nil {
-		panic("oto.NewContext failed: " + err.Error())
-	}
-
-	const timeout = 5 * time.Second
-	select {
-	case <-readych:
-		return samples, audioctx, nil
-	case <-time.After(timeout):
-		break
-	}
-
-	return nil, nil, fmt.Errorf("audio context not ready after %s", timeout)
-}
-
-// Can't fail, always fallback to primary/default monitor.
-// Use 0 for primary monitor.
-func setMonitor(idxmon uint) {
-	monitors := ebiten.AppendMonitors(nil)
-	selidx := 0
-	for i, m := range monitors {
-		modUI.InfoZ("Detected monitor").Int("idx", i).String("name", m.Name()).End()
-		if i == int(idxmon) {
-			selidx = i
-		}
-	}
-
-	ebiten.SetMonitor(monitors[selidx])
-	modUI.InfoZ("Using monitor").Int("idx", selidx).String("name", monitors[selidx].Name()).End()
+type state interface {
+	createUI()
+	update()
+	draw(screen *ebiten.Image)
+	enter(...any)
+	exit()
 }
 
 type appState interface {
@@ -143,8 +52,8 @@ type app struct {
 	displayWidth  int
 	displayHeight int
 
-	states   map[string]appState
-	curstate appState
+	states   map[string]state
+	curstate state
 
 	inputsys     einput.System
 	inputhandler *einput.Handler
@@ -153,7 +62,7 @@ type app struct {
 func newApp(ctx context.Context, samples *sampleBuffer, audioctx *oto.Context, cfg config.Config) *app {
 	app := &app{
 		cfg:           cfg,
-		states:        map[string]appState{},
+		states:        map[string]state{},
 		displayWidth:  startwidth,
 		displayHeight: startheight,
 		samples:       samples,
---
+++ ui/ui.go
@@ -0,0 +1,111 @@
+package ui
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/ebitengine/oto/v3"
+	"github.com/hajimehoshi/ebiten/v2"
+
+	"nestor/config"
+	"nestor/emu/log"
+	"nestor/hw/apu"
+)
+
+var modUI = log.NewModule("ui")
+
+func StartUI(ctx context.Context, cfg config.Config) error {
+	return entrypoint(ctx, cfg, "")
+}
+
+func StartROM(ctx context.Context, cfg config.Config, romPath string) error {
+	return entrypoint(ctx, cfg, romPath)
+}
+
+const startwidth = 800
+const startheight = 600
+
+func entrypoint(ctx context.Context, cfg config.Config, romPath string) error {
+	initResources()
+
+	// Init audio.
+	samples, audioctx, err := initAudio()
+	if err != nil {
+		return fmt.Errorf("initAudio failure: %s", err)
+	}
+
+	// Init video.
+	setMonitor(cfg.Video.Monitor)
+	ebiten.SetWindowTitle("Nestor")
+	ebiten.SetWindowSize(startwidth, startheight)
+	ebiten.SetWindowSizeLimits(startwidth, startheight, -1, -1)
+	ebiten.SetRunnableOnUnfocused(false)
+	if cfg.Video.StartFullscreen {
+		ebiten.SetFullscreen(true)
+	}
+	ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
+	ebiten.SetTPS(ebiten.SyncWithFPS)
+	ebiten.SetRunnableOnUnfocused(true)
+
+	app := newApp(ctx, samples, audioctx, cfg)
+
+	if romPath != "" {
+		app.setState("running")
+		if err := app.runRom(romPath); err != nil {
+			return fmt.Errorf("can't run rom: %w", err)
+		}
+	} else {
+		app.setState("main")
+	}
+
+	options := &ebiten.RunGameOptions{
+		SingleThread: false,
+	}
+	if err := ebiten.RunGameWithOptions(app, options); err != nil {
+		return fmt.Errorf("ui failure: %w", err)
+	}
+
+	modUI.InfoZ("ui quitted").End()
+	return nil
+}
+
+func initAudio() (*sampleBuffer, *oto.Context, error) {
+	const audioBufferSize = 1024 // TODO: adjust based on latency.
+	samples := newSampleBuffer(audioBufferSize)
+
+	audioctx, readych, err := oto.NewContext(&oto.NewContextOptions{
+		SampleRate:   apu.MaxSampleRate,
+		ChannelCount: 2,
+		Format:       oto.FormatSignedInt16LE,
+	})
+	if err != nil {
+		panic("oto.NewContext failed: " + err.Error())
+	}
+
+	const timeout = 5 * time.Second
+	select {
+	case <-readych:
+		return samples, audioctx, nil
+	case <-time.After(timeout):
+		break
+	}
+
+	return nil, nil, fmt.Errorf("audio context not ready after %s", timeout)
+}
+
+// Can't fail, always fallback to primary/default monitor.
+// Use 0 for primary monitor.
+func setMonitor(idxmon uint) {
+	monitors := ebiten.AppendMonitors(nil)
+	selidx := 0
+	for i, m := range monitors {
+		modUI.InfoZ("Detected monitor").Int("idx", i).String("name", m.Name()).End()
+		if i == int(idxmon) {
+			selidx = i
+		}
+	}
+
+	ebiten.SetMonitor(monitors[selidx])
+	modUI.InfoZ("Using monitor").Int("idx", selidx).String("name", monitors[selidx].Name()).End()
+}
