23c2fed49113d445d62f97b7cdb2df7dc6606895
Aurélien Rainone <aurelien.rainone@gmail.com>
2025-12-20 15:25:27 +0100

    emu: move config-related types and functions in config.go

--- emu/emu.go
+++ emu/emu.go
@@ -4,56 +4,12 @@ import (
 	"bytes"
 	"fmt"
 	"image/png"
-	"io"
-	"slices"
 	"sync/atomic"
 
 	"nestor/emu/log"
 	"nestor/hw"
-	"nestor/hw/hwinput"
 	"nestor/ines"
-	"nestor/ui/shader"
 )
-
-type Config struct {
-	Input     hwinput.Config    `toml:"input"`
-	Video     VideoConfig     `toml:"video"`
-	Audio     AudioConfig     `toml:"audio"`
-	Emulation EmulationConfig `toml:"emulation"`
-
-	TraceOut io.WriteCloser `toml:"-"`
-}
-
-type EmulationConfig struct {
-	RunAheadFrames uint `toml:"run_ahead_frames"`
-}
-
-func (ecfg *EmulationConfig) Check() {
-	// Max out the number of run-ahead frames to 10.
-	ecfg.RunAheadFrames = min(ecfg.RunAheadFrames, 10)
-}
-
-type VideoConfig struct {
-	VSync           bool   `toml:"vsync"`
-	StartFullscreen bool   `toml:"start_fullscreen"`
-	Monitor         uint   `toml:"monitor"`
-	Shader          string `toml:"shader"`
-}
-
-func (vcfg *VideoConfig) Check() {
-	// Ensure we have a valid shader.
-	if vcfg.Shader == "" {
-		vcfg.Shader = shader.Default
-	}
-	if !slices.Contains(shader.Names(), vcfg.Shader) {
-		log.ModEmu.Warnf("Invalid shader name %q, fallback to %q", vcfg.Shader, shader.Default)
-		vcfg.Shader = shader.Default
-	}
-}
-
-type AudioConfig struct {
-	DisableAudio bool `toml:"disable_audio"`
-}
 
 type Emulator struct {
 	NES *NES
